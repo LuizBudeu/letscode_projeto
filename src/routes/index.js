34ac7cad6364d8f526b31c3c5b9a20b6fcd554d4
Luiz Budeu <lugui@usp.br>
2022-04-12 19:49:49 -0300

    testando nova branch

---
+++ src/routes/index.js
@@ -0,0 +1,3 @@
+export {default as productRouter} from './products.js';
+export {default as streamRouter} from './stream.js';
+export {default as canalRouter} from './canal.js';
---
+++ src/routes/products.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+
+const router = new Router();
+
+// GET /products/fisico?pageSize=30
+router.get('/:tipo', (req, res) => { // o '?' em '/:tipo?' quer dizer q é optativo
+  console.log(req.query.pageSize);
+  console.log(req.params.tipo);
+
+  res
+    .status(200)
+    .json(req.body);
+});
+
+router.post('/', (req, res) => {
+  res
+    .status(200)
+    .json(req.body);
+});
+
+router.patch('/:id', (req, res) => {
+  //alteraMeuProduto(req.params.id, req.body);
+
+  res
+    .status(200)
+    .json(req.body);
+});
+
+router.delete('/:id', (req, res) => {
+  //deletaMeuProduto(req.params.id, req.body);
+
+  res
+    .status(200);
+});
+
+export default router;
